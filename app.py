f71c483f744b5b1c35bca12c8c29dc144685cb77
Preston Moore <moorepjay@gmail.com>
2023-03-28 22:10:24 -0400

    Init.

--- app.py
+++ app.py
@@ -9,24 +9,33 @@ import database
 import scraper
 import constants
 
-# Scrape price data from target site for both games.
-scraper.get_data(constants.LOST_ARK_SERVER_LIST, constants.NEW_WORLD_SERVER_LIST)
+# If project folders don't exist create them.
+if not os.path.isdir(constants.SOURCE_FOLDER):
+    # Create folders
+    os.mkdir(constants.SOURCE_FOLDER)
+    os.mkdir(constants.CLEANED_SOURCE_FOLDER)
+else:
+    # If there are folders continue.
+    # Scrape price data from target site for both games.
+    scraper.get_data(constants.LOST_ARK_SERVER_LIST, constants.NEW_WORLD_SERVER_LIST)
 
-# Format scraped data for dashboard.
-formatter.clean_folders()
+    # Format scraped data for dashboard.
+    formatter.clean_folders()
 
-# If there isn't a master file continue as usual.
-if not os.path.isfile(constants.NEW_WORLD_CLEANED_MASTER_FILE):
+    # If there isn't a master file continue as usual.
+    if not os.path.isfile(constants.NEW_WORLD_CLEANED_MASTER_FILE):
+        # Aggregate cleaned documents into master for each game.
+        formatter.update_clean_master_files()
+    else:
+        # If there is a master we assume both exist and delete.
+        os.remove(constants.NEW_WORLD_CLEANED_MASTER_FILE)
+        os.remove(constants.LOST_ARK_CLEANED_MASTER_FILE)
+        formatter.update_clean_master_files()
+
     # Aggregate cleaned documents into master for each game.
     formatter.update_clean_master_files()
-else:
-    # If there is a master we assume both exist and delete.
-    os.remove(constants.NEW_WORLD_CLEANED_MASTER_FILE)
-    os.remove(constants.LOST_ARK_CLEANED_MASTER_FILE)
-    formatter.update_clean_master_files()
 
-# Aggregate cleaned documents into master for each game.
-formatter.update_clean_master_files()
+    # Dump our new documents in mongodb.
+    database.upload_csv_to_mongo('PROD')
 
-# Dump our new documents in mongodb.
-database.upload_csv_to_mongo('PROD')
+
